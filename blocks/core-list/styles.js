8bf5b1b3b782e5f39a77261f28bef9acb5c6cbd5
hanneslsm <HannesLeismann@gmail.com>
2023-10-26 00:49:30 +0200

    add list styles: checkmark and crossmark

---
+++ blocks/core-list/styles.js
@@ -0,0 +1,16 @@
+/** List
+ * @since ProLooks 3
+ * --------------------------------------------- */
+
+/**
+ * Register block styles
+ */
+wp.blocks.registerBlockStyle("core/list", {
+  name: "list-checkmark",
+  label: "Checkmark",
+});
+
+wp.blocks.registerBlockStyle("core/list", {
+  name: "list-crossmark",
+  label: "Cross mark",
+});
