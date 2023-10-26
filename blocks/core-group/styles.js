0085c60babe26103f54a99b09bc59b816534a620
hanneslsm <HannesLeismann@gmail.com>
2023-10-27 00:06:31 +0200

    Add shadow group style

---
+++ blocks/core-group/styles.js
@@ -0,0 +1,11 @@
+/** Group
+ * @since ProLooks 3
+ * --------------------------------------------- */
+
+/**
+ * Register block styles
+ */
+wp.blocks.registerBlockStyle("core/group", {
+  name: "group-hover-shadow",
+  label: "Hover: Shadow Medium",
+});
