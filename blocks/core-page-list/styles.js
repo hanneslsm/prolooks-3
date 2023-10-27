3f5e53339612eb975db15368ac157807bff14c05
hanneslsm <HannesLeismann@gmail.com>
2023-10-28 00:20:47 +0200

    Add style variation

---
+++ blocks/core-page-list/styles.js
@@ -0,0 +1,11 @@
+/** Page List
+ * @since ProLooks 3
+ * --------------------------------------------- */
+
+/**
+ * Register block styles
+ */
+wp.blocks.registerBlockStyle("core/page-list", {
+  name: "page-list-plain",
+  label: "Plain",
+});
