1ab30e05d83ead7537e6054584a1b61e34685d6b
hanneslsm <HannesLeismann@gmail.com>
2023-10-27 19:01:26 +0200

    Correct CTA

--- patterns/CTA-contact-us.php
+++ patterns/CTA-contact-us.php
@@ -6,14 +6,14 @@
  * Viewport width: 1280
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|distant","bottom":"var:preset|spacing|distant","left":"var:preset|spacing|default","right":"var:preset|spacing|default"}},"border":{"radius":"16px"}},"backgroundColor":"brand-light-default","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide has-brand-light-default-background-color has-background" style="border-radius:16px;padding-top:var(--wp--preset--spacing--distant);padding-right:var(--wp--preset--spacing--default);padding-bottom:var(--wp--preset--spacing--distant);padding-left:var(--wp--preset--spacing--default)"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|brand-dark-default"}}}},"textColor":"brand-dark-default"} -->
-<h2 class="wp-block-heading has-text-align-center has-brand-dark-default-color has-text-color has-link-color"><?php esc_html_e( 'Haben Sie Fragen oder möchten Sie mehr erfahren?', 'prolooks' ); ?></h2>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|distant","bottom":"var:preset|spacing|distant"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--distant);margin-bottom:var(--wp--preset--spacing--distant)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|distant","bottom":"var:preset|spacing|distant","left":"var:preset|spacing|default","right":"var:preset|spacing|default"}},"border":{"radius":"16px","width":"1px"},"background":{"backgroundImage":{"url":"http://prolooks.local/wp-content/uploads/2023/10/grid-bg-2-1920px.png","id":2533,"source":"file","title":"grid-bg-2-1920px"}}},"borderColor":"brand-light-darkest","backgroundColor":"brand-light-default","className":"is-style-default","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide is-style-default has-border-color has-brand-light-darkest-border-color has-brand-light-default-background-color has-background" style="border-width:1px;border-radius:16px;padding-top:var(--wp--preset--spacing--distant);padding-right:var(--wp--preset--spacing--default);padding-bottom:var(--wp--preset--spacing--distant);padding-left:var(--wp--preset--spacing--default)"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|brand-dark-default"}}}},"textColor":"brand-dark-default"} -->
+<h2 class="wp-block-heading has-text-align-center has-brand-dark-default-color has-text-color has-link-color">Möchten Sie mehr erfahren oder haben Sie Fragen?</h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><?php esc_html_e( 'Sprechen Sie mit uns! Wir beraten Sie gerne unverbindlich oder erstellen Ihnen ein individuelles Angebot.', 'prolooks' ); ?></p>
+<!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size">Sprechen Sie mit uns! Wir beraten Sie gerne unverbindlich oder <br>erstellen Ihnen ein individuelles Angebot.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:spacer {"height":"var:preset|spacing|default"} -->
@@ -22,36 +22,36 @@
 
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|relaxed","bottom":"var:preset|spacing|relaxed","left":"var:preset|spacing|relaxed","right":"var:preset|spacing|relaxed"}},"border":{"radius":"8px"}},"backgroundColor":"light-lightest","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-light-lightest-background-color has-background" style="border-radius:8px;padding-top:var(--wp--preset--spacing--relaxed);padding-right:var(--wp--preset--spacing--relaxed);padding-bottom:var(--wp--preset--spacing--relaxed);padding-left:var(--wp--preset--spacing--relaxed)"><!-- wp:image {"align":"center","id":13,"width":"24px","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_theme_file_uri( 'wp-content/uploads/2023/07/ProLooks-Favion@2x.png' ) ); ?>" alt="<?php echo esc_attr__( 'ProLooks Favion', 'prolooks' ); ?>" class="wp-image-13" style="width:24px"/></figure>
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|relaxed","bottom":"var:preset|spacing|relaxed","left":"var:preset|spacing|relaxed","right":"var:preset|spacing|relaxed"}},"border":{"radius":"8px","width":"1px"}},"borderColor":"brand-light-darker","backgroundColor":"light-lightest","className":"is-style-group-hover-shadow","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-group-hover-shadow has-border-color has-brand-light-darker-border-color has-light-lightest-background-color has-background" style="border-width:1px;border-radius:8px;padding-top:var(--wp--preset--spacing--relaxed);padding-right:var(--wp--preset--spacing--relaxed);padding-bottom:var(--wp--preset--spacing--relaxed);padding-left:var(--wp--preset--spacing--relaxed)"><!-- wp:image {"align":"center","id":31,"width":"24px","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><img src="/assets/icons/phone.svg" alt="" class="wp-image-31" style="width:24px"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:group {"style":{"spacing":{"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|brand-dark-default"}}}},"textColor":"brand-dark-default"} -->
-<p class="has-text-align-center has-brand-dark-default-color has-text-color has-link-color"><strong><?php esc_html_e( 'Telefon', 'prolooks' ); ?></strong></p>
+<p class="has-text-align-center has-brand-dark-default-color has-text-color has-link-color"><strong>Telefon</strong></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center">+49 6274 928 099 0</p>
+<p class="has-text-align-center"><a href="tel:+4962749280990" data-type="tel" data-id="tel:+4962749280990">+49 6274 928 099 0</a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|relaxed","bottom":"var:preset|spacing|relaxed","left":"var:preset|spacing|relaxed","right":"var:preset|spacing|relaxed"}},"border":{"radius":"8px"}},"backgroundColor":"light-lightest","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-light-lightest-background-color has-background" style="border-radius:8px;padding-top:var(--wp--preset--spacing--relaxed);padding-right:var(--wp--preset--spacing--relaxed);padding-bottom:var(--wp--preset--spacing--relaxed);padding-left:var(--wp--preset--spacing--relaxed)"><!-- wp:image {"align":"center","id":13,"width":"24px","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_theme_file_uri( 'wp-content/uploads/2023/07/ProLooks-Favion@2x.png' ) ); ?>" alt="<?php echo esc_attr__( 'ProLooks Favion', 'prolooks' ); ?>" class="wp-image-13" style="width:24px"/></figure>
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|relaxed","bottom":"var:preset|spacing|relaxed","left":"var:preset|spacing|relaxed","right":"var:preset|spacing|relaxed"}},"border":{"radius":"8px","width":"1px"}},"borderColor":"brand-light-darker","backgroundColor":"light-lightest","className":"is-style-group-hover-shadow","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-group-hover-shadow has-border-color has-brand-light-darker-border-color has-light-lightest-background-color has-background" style="border-width:1px;border-radius:8px;padding-top:var(--wp--preset--spacing--relaxed);padding-right:var(--wp--preset--spacing--relaxed);padding-bottom:var(--wp--preset--spacing--relaxed);padding-left:var(--wp--preset--spacing--relaxed)"><!-- wp:image {"align":"center","id":32,"width":"24px","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><img src="/assets/icons/envelope.svg" alt="" class="wp-image-32" style="width:24px"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:group {"style":{"spacing":{"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|brand-dark-default"}}}},"textColor":"brand-dark-default"} -->
-<p class="has-text-align-center has-brand-dark-default-color has-text-color has-link-color"><strong><?php esc_html_e( 'E-Mail', 'prolooks' ); ?></strong></p>
+<p class="has-text-align-center has-brand-dark-default-color has-text-color has-link-color"><strong>E-Mail</strong></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center">info@linktec.com</p>
+<p class="has-text-align-center"><a href="mailto:info@linktec.com">info@linktec.com</a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
@@ -59,5 +59,3 @@
 <!-- /wp:columns --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
-
-
